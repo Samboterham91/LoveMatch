4197af2e595ebf7fb3e86e6bc3ed1e3a47bad875
Samboterham91 <emchermans@gmail.com>
2026-02-25 11:00:48 +0100

    Project aangemaakt

---
+++ LoveMatch/LoveMatch/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "LoveMatch")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "LoveMatch.Pages")]

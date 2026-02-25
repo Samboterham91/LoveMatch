a8bf84471df38592c32624dfdc8b70a00e60f9fe
Samboterham91 <emchermans@gmail.com>
2026-02-25 11:43:30 +0100

    Klasse Member aangemaakt

---
+++ Oscar/LoveMatch/LoveMatch/Models/Member.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Project van Oscar
+namespace LoveMatch.Models
+{
+    internal class Member
+    {
+    }
+}

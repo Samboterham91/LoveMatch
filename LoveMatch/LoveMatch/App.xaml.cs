6febdee2288e512c623f19b5c655f306506e921a
Samboterham91 <emchermans@gmail.com>
2026-03-04 11:53:41 +0100

    Main gevuld vanuit backup
    - MAUI app
    - gitignore
    - git workflow

---
+++ LoveMatch/LoveMatch/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace LoveMatch
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            // Start met BioSelectionPage in NavigationPage
+            MainPage = new NavigationPage(new BioSelectionPage());
+        }
+    }
+}

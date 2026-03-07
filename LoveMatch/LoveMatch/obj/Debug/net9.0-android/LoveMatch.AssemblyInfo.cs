acbda7d9806e7bb8341dfff6f5886662f84a95ee
Tim Dekker <211560602+Dek-T1995@users.noreply.github.com>
2026-03-08 00:12:36 +0100

    /

--- LoveMatch/LoveMatch/obj/Debug/net9.0-android/LoveMatch.AssemblyInfo.cs
+++ LoveMatch/LoveMatch/obj/Debug/net9.0-android/LoveMatch.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LoveMatch")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+dfbee3a93212f72e28067363b289c044259aeb88")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+8ad44ace5f023975ef63aaf7c62c1f2d3aba98a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("LoveMatch")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LoveMatch")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
